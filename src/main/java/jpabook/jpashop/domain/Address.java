5828b2877e6576b76740309cd741d31409d4d6f6
seokil <seokil1220@gmail.com>
2021-07-20 22:02:16 +0900

    Develop OrderService

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,21 +1,18 @@
 package jpabook.jpashop.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Getter
+@Getter @Setter
+//@AllArgsConstructor
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
-
-    public Address(String city, String street, String zipcode) {
-        this.city = city;
-        this.street = street;
-        this.zipcode = zipcode;
-    }
 }
