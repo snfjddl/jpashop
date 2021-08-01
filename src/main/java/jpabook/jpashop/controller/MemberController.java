d0c7cbe5225071f7626b00c46d412154d3b5700d
seokil <seokil1220@gmail.com>
2021-08-01 14:47:16 +0900

    final JPA prac

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -41,6 +41,13 @@ public class MemberController {
         return "redirect:/";
     }
 
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 //    @GetMapping("/members/list")
 //    public List<Member> retreive() {
 //
